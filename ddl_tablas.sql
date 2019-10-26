7a905c0e3cf4a07f9e5103acfc5f76f819f794fd
Katy <katyrl@hotmail.com>
2019-10-26 11:50:18 -0500

    quinto comentario

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,11 +1,14 @@
 -- creacion de tablas sql
 -- autor : Katherine
 --trabajando en tabla alterna -- desarrollo - etiqueta nro 4
+--comentario
 
 create table td_cliente (
 nombre varchar2(50)
 apellidos varchar2(50),
-direccion varcahr2(100))
+direccion varchar2(100),
+seccion varchar2(20)
+)
 
 tablespace tmp;
 
