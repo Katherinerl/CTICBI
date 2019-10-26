24ce4572bdb724e63722dd2c92e45cb50c1e6087
Katy <katyrl@hotmail.com>
2019-10-26 11:44:30 -0500

    cuarto cambio

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,7 +1,11 @@
 -- creacion de tablas sql
 -- autor : Katherine
+--trabajando en tabla alterna -- desarrollo - etiqueta nro 4
 
 create table td_cliente (
 nombre varchar2(50)
 apellidos varchar2(50),
-direccion varcahr2(100)
+direccion varcahr2(100))
+
+tablespace tmp;
+
